162ca724cd402781758c6eb925f287b7bfb5b092
Gustavo Lima <gustavo.lima@zocdoc.com>
2022-04-04 18:22:17 +0100

    feat: add pdfobject

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import { data } from "./pdffile";
+/* eslint-disable no-undef */
+import {useRef,useEffect} from 'react';
 
 function App() {
-  // If you are using Acrobat Reader to display the file in the browser, then you could use Acrobat Reader URL parameters.
-  // URL PARAMETERS - https://opensource.adobe.com/dc-acrobat-sdk-docs/
-  // https://pdfobject.com/pdf/pdf_open_parameters_acro8.pdf
-  // https://kb2.adobe.com/jp/cps/511/511759/attachments/511759_pdf_open_parameters.pdf
-  // https://pdfobject.com/
-  // 
-  // EMBED PDF USING ACROBAT READER - https://stackoverflow.com/questions/2474811/how-to-embed-pdf-in-a-web-page-using-acrobat-reader-instead-of-acrobat
-  //
-  // https://stackoverflow.com/questions/5343114/pdf-open-parameters-comment-commentid-doesnt-work
-  //
-  // ref: https://stackoverflow.com/questions/15556210/display-pdf-in-web-page-and-scroll-it-programmatically
-  const goToPage = `#page=3&comment=babuy`;
+  // https://pdfobject.com/#api
+  const pdfobject = useRef(null);
+  useEffect(() => {
+    if(pdfobject.current){
+      PDFObject.embed("file.pdf", pdfobject.current);
+    }
+  }, [pdfobject.current]);
   return (
-    <div className="App">
-      <iframe
-        src={`${data}${goToPage}`}
-        width="100%"
-        height="500px"
-        title="PDF File"
-      />
-    </div>
+    <div id="example1" ref={pdfobject} style={{width:'100%',height:'500px'}} />
   );
 }
 
