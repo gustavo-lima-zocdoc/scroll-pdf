4dfeaff60425c666b13a08be2dee82c373e7491c
Gustavo Lima <gustavo.lima@zocdoc.com>
2022-04-05 10:16:56 +0100

    feat: scroll to annotation

--- src/App.js
+++ src/App.js
@@ -18,12 +18,46 @@ function App() {
     WebViewer(
       {
         path: '/lib',
+        fullAPI: true,
       },
       viewer.current,
     ).then((instance) => {
-        const { UI, Core: { documentViewer } } = instance;
-        
+        const { UI, Core: { Annotations, annotationManager, documentViewer } } = instance;
+
+        // UI.disableElement('searchButton');
+        // UI.setTheme('dark');
+        // UI.enableElements(['readerPageTransitionButton']);
+
+        const { disableFeatures, Feature } = instance.UI;
+        disableFeatures([Feature.Download]);
+        disableFeatures([Feature.Print]);
+        disableFeatures([Feature.FilePicker]);
+        disableFeatures([Feature.NotesPanel]);
+        disableFeatures([Feature.Annotations]);
+        disableFeatures([Feature.TextSelection]);
+        disableFeatures([Feature.Ribbons]);
+
+        annotationManager.setPermissionCheckCallback(() => false);
+
+
         UI.loadDocument(base64ToBlob(data), { filename: `baby.pdf` });
+        documentViewer.addEventListener('documentLoaded', () => {
+          UI.setFitMode(UI.FitMode.FitWidth);
+          
+          const rectangleAnnot = new Annotations.RectangleAnnotation();
+          rectangleAnnot.PageNumber = 2;
+          // values are in page coordinates with (0, 0) in the top left
+          rectangleAnnot.X = 100;
+          rectangleAnnot.Y = 550;
+          rectangleAnnot.Width = 200;
+          rectangleAnnot.Height = 50;
+    
+          annotationManager.addAnnotation(rectangleAnnot);
+          // need to draw the annotation otherwise it won't show up until the page is refreshed
+          annotationManager.redrawAnnotation(rectangleAnnot);
+
+          annotationManager.jumpToAnnotation(rectangleAnnot);
+        })
       });
   }, []);
   return (
